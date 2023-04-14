d734364a4da1eb811dda9b0faf4effc7b5dafb51
LIY-Cat <mcpe9061@naver.com>
2023-04-14 12:03:21 +0900

    20230414

---
+++ SpringProject6_Jdbc_mysql_step3/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -0,0 +1,38 @@
+package com.springbook.biz.board.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springbook.biz.board.BoardService;
+import com.springbook.biz.board.BoardVO;
+
+@Service("boardService")
+public class BoardServiceImpl implements BoardService {
+	@Autowired
+	private BoardDAOSpring boardDAO;
+	
+	@Transactional("txManager")
+	public void insertBoard(BoardVO vo) {
+  		boardDAO.insertBoard(vo); // 100踰� 湲� �벑濡� �꽦怨�
+		boardDAO.insertBoard(vo); // Exception 諛쒖깮
+	}
+
+	public void updateBoard(BoardVO vo) {
+		boardDAO.updateBoard(vo);
+	}
+
+	public void deleteBoard(BoardVO vo) {
+		boardDAO.deleteBoard(vo);
+	}
+
+	public BoardVO getBoard(BoardVO vo) {
+		return boardDAO.getBoard(vo);
+	}
+
+	public List<BoardVO> getBoardList(BoardVO vo) {
+		return boardDAO.getBoardList(vo);
+	}
+}
