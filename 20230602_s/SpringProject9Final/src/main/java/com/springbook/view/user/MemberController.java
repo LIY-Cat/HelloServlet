54bd94d0b5207f42c12ca459c2b5f812e325f960
LIY-Cat <mcpe9061@naver.com>
2023-06-02 13:01:07 +0900

    20230602_!!

---
+++ 20230602_s/SpringProject9Final/src/main/java/com/springbook/view/user/MemberController.java
@@ -0,0 +1,44 @@
+﻿package com.springbook.view.user;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.springbook.biz.user.UserService;
+import com.springbook.biz.user.UserVO;
+
+@Controller
+public class MemberController {
+	@Autowired
+	private UserService userService;
+
+	// login
+	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
+	public String loginView(@ModelAttribute("user") UserVO vo) {
+		System.out.println("로그인 화면으로 이동...");
+		vo.setId("t1");
+		vo.setPassword("1");
+		return "login.jsp";
+	}
+
+	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
+	public String login(UserVO vo, HttpSession session) {
+		UserVO user = userService.getUser(vo);
+		if (user != null) {
+			session.setAttribute("userName", user.getName());
+			return "getBoardList.do";
+		} else
+			return "login.jsp";
+	}
+
+	// logout
+	@RequestMapping("/logout.do")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "login.jsp";
+	}
+}
